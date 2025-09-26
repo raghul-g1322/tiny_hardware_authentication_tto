e42009ff7343d1c4d493db3777aff26e6a1dce1d
Raghul Ganesan <139033229+raghul-g1322@users.noreply.github.com>
2025-09-26 14:32:26 +0530

    Update clk_div_2sec.v

--- src/clk_div_2sec.v
+++ src/clk_div_2sec.v
@@ -3,21 +3,22 @@ module clk_div_2sec(
     output reg clk_out_disp2
 );
 
-    reg [26:0] ct2;
+    reg [25:0] ct2;
     
-    parameter DISP = 100_000_000;
+    parameter DISP = 50_000_000;
     
     always @(posedge clk or negedge rstn) begin
         if(!rstn) begin
-            ct2 <= 27'd0;
+            ct2 <= 26'd0;
             clk_out_disp2 <= 1'b0;
         end else begin
             if (ct2 == DISP-1) begin
-                ct2 <= 27'd0;
+                ct2 <= 26'd0;
                 clk_out_disp2 <= ~clk_out_disp2;
             end else begin
-                ct2 <= ct2 + 27'd1;
+                ct2 <= ct2 + 26'd1;
             end
         end
     end
+
 endmodule
