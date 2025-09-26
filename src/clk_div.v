497a1d6ea89d1be4bcc8f79e5301663871e349f1
Raghul Ganesan <139033229+raghul-g1322@users.noreply.github.com>
2025-09-26 14:29:07 +0530

    Update clk_div.v

--- src/clk_div.v
+++ src/clk_div.v
@@ -3,21 +3,22 @@ module clk_div(
     output reg clk_out_disp
 );
 
-    reg [14:0] ct2;
+    reg [13:0] ct2;
     
-    parameter DISP = 25_000;
+    parameter DISP = 12_500;
     
     always @(posedge clk or negedge rstn) begin
         if(!rstn) begin
-            ct2 <= 15'd0;
+            ct2 <= 14'd0;
             clk_out_disp <= 1'b0;
         end else begin
             if (ct2 == DISP-1) begin
-                ct2 <= 15'd0;
+                ct2 <= 14'd0;
                 clk_out_disp <= ~clk_out_disp;
             end else begin
-                ct2 <= ct2 + 15'd1;
+                ct2 <= ct2 + 14'd1;
             end
         end
     end
+
 endmodule
